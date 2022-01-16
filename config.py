f17a503954cdeadd6b6331164a347f118580ccbd
Dhruvjika <97802246+Dhruvjika@users.noreply.github.com>
2022-01-16 23:56:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import  os
 
-BOT_TOKEN = os.environ.get("BOT_TOKEN")
-APP_ID = int(os.environ.get("API_ID"))
-API_HASH = os.environ.get("API_HASH")
+BOT_TOKEN = os.environ.get("5086619049:AAHTBqXyXhVfXh_LX52aa5xB6sXsTw7GUO8BOT_TOKEN")
+APP_ID = int(os.environ.get("18505100API_ID"))
+API_HASH = os.environ.get("c0e38c4dd4200cead73bdf13d1943cc5API_HASH")
 
 youtube_next_fetch = 0  # time in minute
 
 
-EDIT_TIME = 5
+EDIT_TIME =
